a6968e8662ccef12a0fbd21729f9567aaeaff06f
Alan Račić <alan.racic@gmail.com>
2026-05-07 10:35:26 +0200

    Configured Swagger and prepared API for Azure deployment

--- WeatherGatewayAPI/Program.cs
+++ WeatherGatewayAPI/Program.cs
@@ -9,14 +9,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
-builder.Services.Configure<ExternalWeatherApiSettings>(builder.Configuration.GetSection("ExternalWeatherApi"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddHttpClient<IExternalService, ExternalService>((serviceProvider, client)=> 
+builder.Services.Configure<ExternalWeatherApiSettings>(
+    builder.Configuration.GetSection("ExternalWeatherApi"));
+
+builder.Services.AddHttpClient<IExternalService, ExternalService>((serviceProvider, client) =>
 {
-    var settings = serviceProvider.GetRequiredService<Microsoft.Extensions.Options.IOptions<ExternalWeatherApiSettings>>().Value;
+    var settings = serviceProvider
+        .GetRequiredService<Microsoft.Extensions.Options.IOptions<ExternalWeatherApiSettings>>()
+        .Value;
 
     client.BaseAddress = new Uri(settings.BaseUrl);
 
@@ -26,27 +30,36 @@ builder.Services.AddHttpClient<IExternalService, ExternalService>((serviceProvid
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 var key = jwtSettings["Key"];
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options=>options.TokenValidationParameters = new TokenValidationParameters 
-    { ValidateIssuer = true,
-      ValidateAudience = true,
-      ValidateLifetime = true,
-      ValidateIssuerSigningKey = true,
-      ValidIssuer = jwtSettings["Issuer"],
-      ValidAudience = jwtSettings["Audience"],
-      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!))
-});
+if (string.IsNullOrWhiteSpace(key))
+{
+    throw new InvalidOperationException("JWT Key is missing from configuration.");
+}
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(key))
+        });
 
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
